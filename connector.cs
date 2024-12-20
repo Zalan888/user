ba7acc24af9ea8473c2f05db48cdd45207a85b31
Zalan888 <witmannz@kkszki.hu>
2024-12-20 09:53:10 +0100

    Added .sql and the connector for the DB

---
+++ connector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace user
+{
+    internal class Connector
+    {
+    
+        public static void Connect()
+        {
+            // connect to the database on localhost
+            string connString = "Server=localhost;Port=3306";
+            MySqlConnection conn = new MySqlConnection(connString);
+            try
+            {
+                conn.Open();
+                Console.WriteLine("Connected to the database");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            
+        }
+    }
+}
